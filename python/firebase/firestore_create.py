1b012c753ab52a87d2acc0b32f0c644aac277828
koooota <eno6k21@gmail.com>
2021-09-02 01:02:31 +0900

    Post json to Firebase

--- python/firebase/firestore_create.py
+++ python/firebase/firestore_create.py
@@ -1,14 +1,22 @@
 #! /usr/bin/python
 import sys
-import datetime
+from datetime import date, datetime
 import random, string
 import json
+from typing import NewType
 import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import firestore
 
 def random_key(n):
    return ''.join(random.choices(string.ascii_letters + string.digits, k=n))
+
+def convert_timestamp(datetimeStr):  
+    # datetimenow = datetime.now()
+    dt = [int(i) for i in datetimeStr.split('/')]
+    datetimenow = datetime(dt[0], dt[1], dt[2], dt[3], dt[4], dt[5], dt[6])
+
+    return datetimenow
 
 def load_json(json_data):
     json_open = open(json_data, 'r')
@@ -17,11 +25,26 @@ def load_json(json_data):
     return json_list
 
 def add_data(db, collection_name, json_path, uid, key_len):
-    json_list = load_json(json_path)
-    [input_data.update({uid: random_key(key_len)}) for input_data in json_list]
 
-    doc_ref = db.collection(collection_name)
-    [doc_ref.document(input_data[uid]).set(input_data) for input_data in json_list]
+    if collection_name == 'messages': 
+        json_list = load_json(json_path)
+        [input_data.update(
+            {
+                uid: random_key(key_len), 
+                'create_time': convert_timestamp(input_data['create_time']),
+                'update_time': convert_timestamp(input_data['update_time']),
+            }
+        ) for input_data in json_list]
+
+        doc_ref = db.collection(collection_name)
+        [doc_ref.document(input_data[uid]).set(input_data) for input_data in json_list]
+    
+
+    # json_list = load_json(json_path)
+    # [input_data.update({uid: random_key(key_len)}) for input_data in json_list]
+
+    # doc_ref = db.collection(collection_name)
+    # [doc_ref.document(input_data[uid]).set(input_data) for input_data in json_list]
 
 
 if __name__ == '__main__':
